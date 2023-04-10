2f04728b231d90caae6ad97df497fd3ddd1922f6
João Ferreira <joao.ferreira@fullfabric.com>
2023-04-10 13:07:42 +0100

    Chores: add import rules

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,32 @@
 module.exports = {
-  singleQuote: true,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxSingleQuote: true,
   semi: false,
-  trailingComma: 'none'
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'none',
+
+  // @ianvs/prettier-plugin-sort-imports
+  importOrder: [
+    '^react(.*)',
+    '',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^@(?!fullfabric).+',
+    '^@fullfabric/(.*)$',
+    '',
+    '^spec/',
+    '',
+    '^(api|context|hooks|constants|utils)/?',
+    '^(components|pages)/?',
+    '^[./].*(?<![.]s?css)$',
+    '',
+    '^classnames&',
+    '[.]s?css$'
+  ],
+  importOrderSeparation: false,
+  importOrderSortSpecifiers: true,
+  importOrderMergeDuplicateImports: true
 }
