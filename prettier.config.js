fb0201c9d5541dfca6f62555d9339a838cda3573
fplgusmao <fplgusmao@users.noreply.github.com>
2023-09-11 19:00:39 +0100

    fix sorting for spec files

--- prettier.config.js
+++ prettier.config.js
@@ -10,21 +10,21 @@ module.exports = {
 
   // @ianvs/prettier-plugin-sort-imports
   importOrder: [
+    '', // to add a gap at the top after top-of-file-comments (automatically removed when appropriate)
     '^react(.*)',
     '',
     '<THIRD_PARTY_MODULES>',
     '',
+    '^@(?!fullfabric).+',
     '^@fullfabric/(.*)$',
     '',
-    '^spec/',
+    '^(.+/)*spec', // spec files
     '',
     '^(api|context|hooks|constants|utils|components|pages|shared|apps)/?', // src/ imports
     '^[./].*(?<![.]s?css)$', // every other local import except for css files
     '',
     '^classnames$',
-    '[.]s?css$'
-  ],
-  importOrderSeparation: false,
-  importOrderSortSpecifiers: true,
-  importOrderMergeDuplicateImports: true
+    '[.]s?css$',
+    ''
+  ]
 }
