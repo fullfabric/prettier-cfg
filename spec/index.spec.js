d15f823c356f381485a096d335d7d192b9326d05
Francisco Gusmão <fplgusmao@users.noreply.github.com>
2024-06-18 16:41:26 +0100

    fix error in absolute imports regex

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,3 +1,21 @@
+import React from 'react'
+
+import i18n from 'i18next'
+
+import { render } from '@testing-library/react'
+import { Text } from '@fullfabric/alma-mater'
+
+import renderApp from 'spec/helpers'
+
+import Header from 'components/Header'
+import locales from 'i18n/locales/en'
+import Body from '../Body'
+import Footer from './Footer'
+
+import classNames from 'classnames'
+import footerStyles from './Footer/styles.module.scss'
+import styles from 'components/Header/styles.module.scss'
+
 /**
  * Only used to run prettier against this, mostly as a way to check the config
  * is valid.
